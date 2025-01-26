28d129af4b23491d215e737f870fccb54beb76aa
Sutorenja <Mathiasruller@gmail.com>
2025-01-26 19:59:53 +0100

    add 'has' command to cli

--- cli/cli.go
+++ cli/cli.go
@@ -17,13 +17,13 @@ func Run() error {
 var root = &cli.Command{
 	Description: "",
 	Usage:       "",
-	Commands:    []*cli.Command{get, list},
+	Commands:    []*cli.Command{get, list, has},
 }
 
 var get = &cli.Command{
 	Name:        "get",
 	Description: "",
-	Usage:       "",
+	Usage:       "pls get [youtube playlist url] [flags...]",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{Name: "quiet", Aliases: []string{"q"}, Usage: "do not print info to stderr"},
 	},
@@ -33,7 +33,7 @@ var get = &cli.Command{
 		if !args.Present() {
 			return fmt.Errorf("need playlist url arg")
 		}
-		if err := pls.ValidatePlaylistUrl(args.First()); err != nil {
+		if err := pls.ValidatePlaylistFeedUrl(args.First()); err != nil {
 			return fmt.Errorf("not a valid playlist url: %s", err)
 		}
 
@@ -62,12 +62,16 @@ var get = &cli.Command{
 var list = &cli.Command{
 	Name:        "list",
 	Description: "",
-	Usage:       "",
+	Usage:       "pls list [sqlite file] [flags...]",
 	Flags: []cli.Flag{
 		&cli.StringFlag{Name: "query", Aliases: []string{"q"}, Usage: "fuzzy search query"},
 		&cli.IntFlag{Name: "limit", Aliases: []string{"n"}, Usage: "number of results. Must be a positive integer", Action: NotNegative},
 		&cli.BoolFlag{Name: "tui", Aliases: []string{"t"}, Usage: "launch a tui and browse the result instead of printing it to stdout"},
 		// the tui does not let you filter or search or anything like that (you do that with the cli flags above)
+
+		// TODO add flags that let you specify output format. Instead of printing URL, we could print video title, channel title etc.
+		// in the mean time, just use a url flag
+		&cli.BoolFlag{Name: "url", Usage: "print video url"},
 	},
 	Action: func(ctx context.Context, c *cli.Command) error {
 		args := c.Args()
@@ -100,15 +104,55 @@ var list = &cli.Command{
 		videos = videos[0:min(n, len(videos))]
 
 		if c.Bool("tui") {
-			return pls.TUI(videos)
+			videos, err = pls.TUI(videos)
+			if err != nil {
+				return err
+			}
 		}
-
-		// TODO print URLs???
 
 		for i, video := range videos {
+			if c.Bool("url") {
+				fmt.Println(video.Url())
+				continue
+			}
 			fmt.Printf("%d: %s\n", i+1, video.Title)
 		}
+		return nil
+	},
+}
 
+var has = &cli.Command{
+	Name:        "has",
+	Description: "",
+	Usage:       "pls has [sqlite file] [youtube url]",
+	Action: func(ctx context.Context, c *cli.Command) error {
+		args := c.Args()
+
+		if args.Len() > 2 {
+			return fmt.Errorf("need sqlite database file arg and url arg")
+		}
+		if err := pls.ValidateVideoUrl(args.Get(1)); err != nil {
+			return err
+		}
+		id, err := pls.VideoUrlId(args.Get(1))
+		if err != nil {
+			return err
+		}
+		db, err := pls.DB(args.First())
+		if err != nil {
+			return err
+		}
+		videos, err := pls.GetAllVideos(db)
+		if err != nil {
+			return err
+		}
+		for _, video := range videos {
+			if video.VideoId == id {
+				fmt.Println("true")
+				return nil
+			}
+		}
+		fmt.Println("false")
 		return nil
 	},
 }
